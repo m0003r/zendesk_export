5aceaa10f94f2cb24f6e19f2442096baab7dc198
m0003r <m0003r@gmail.com>
2022-04-14 18:31:29 +0300

    Working exporter

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,10 @@ impl ApiClient {
         self.request_url(&self.make_url(method))
     }
 
+    pub fn get_comments(&self, id: &str) -> Result<JsonValue, ApiError> {
+        self.request_url(&self.make_url(&format!("tickets/{}/comments", id)))
+    }
+
     fn make_url(&self, method: &str) -> String {
         format!("https://{}.zendesk.com/api/v2/{}", self.domain, method)
     }
@@ -48,31 +52,53 @@ impl ApiClient {
             )?
         )
     }
+
+    pub fn get_all_pages(&self, object: &str) -> Vec<JsonValue> {
+        let mut objects: Vec<JsonValue> = vec![];
+        let tickets_iter = ZendeskPageIterator::new(&self, object);
+        for v in tickets_iter {
+            match v {
+                Ok(val) => {
+                    eprint!("Got {} {}", val[object].len(), object);
+                    objects.extend(val[object].members().map(|v| v.clone()));
+
+                    eprintln!(", total {} {}", objects.len(), object);
+                }
+                Err(ref err) => {
+                    eprintln!("Some error: {:?}", err);
+                }
+            }
+        }
+        objects
+    }
+
 }
 
-pub struct TicketIterator<'a> {
+pub struct ZendeskPageIterator<'a> {
     client: &'a ApiClient,
+    method: String,
     page: u32,
     next_page_link: Option<String>,
     finished: bool,
     known_page_size: Option<usize>,
-    total_tickets: Option<u32>,
+    total_size: Option<u32>,
 }
 
-impl<'a> TicketIterator<'a> {
-    pub fn new(client: &'a ApiClient) -> Self {
-        TicketIterator {
+impl<'a> ZendeskPageIterator<'a> {
+    pub fn new(client: &'a ApiClient, method: &str) -> Self {
+        ZendeskPageIterator {
             client,
+            method: String::from(method),
             page: 0,
             next_page_link: None,
             finished: false,
             known_page_size: None,
-            total_tickets: None,
+            total_size: None,
         }
     }
 }
 
-impl<'a> Iterator for TicketIterator<'a> {
+impl<'a> Iterator for ZendeskPageIterator<'a> {
     type Item = Result<JsonValue, ApiError>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -80,7 +106,7 @@ impl<'a> Iterator for TicketIterator<'a> {
             return None;
         }
 
-        let mut req_url = &self.client.make_url("tickets");
+        let mut req_url = &self.client.make_url(&self.method);
         if let Some(url) = &self.next_page_link {
             req_url = &url;
         }
@@ -89,16 +115,16 @@ impl<'a> Iterator for TicketIterator<'a> {
 
         match &resp {
             Ok(val) => {
-                if !val.has_key("tickets") {
-                    Some(Err(OtherError("Tickets elt not found".to_string())))
-                } else if !val["tickets"].is_array() {
-                    Some(Err(OtherError("Tickets are not array".to_string())))
-                } else if val["tickets"].len() == 0 {
-                    Some(Err(OtherError("Tickets len is 0".to_string())))
+                if !val.has_key(&self.method) {
+                    Some(Err(OtherError(format!("{} not found in response", &self.method))))
+                } else if !val[&self.method].is_array() {
+                    Some(Err(OtherError(format!("{} is not an array", &self.method))))
+                } else if val[&self.method].len() == 0 {
+                    Some(Err(OtherError(format!("{} is empty array", &self.method))))
                 } else {
                     if let None = self.known_page_size {
-                        self.known_page_size = Some(val["tickets"].len());
-                        self.total_tickets = val["count"].as_u32()
+                        self.known_page_size = Some(val[&self.method].len());
+                        self.total_size = val["count"].as_u32()
                     }
 
                     self.page += 1;
@@ -116,7 +142,7 @@ impl<'a> Iterator for TicketIterator<'a> {
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        if let (Some(ps), Some(tt)) = (self.known_page_size, self.total_tickets) {
+        if let (Some(ps), Some(tt)) = (self.known_page_size, self.total_size) {
             let size = (tt as usize) / ps;
             (size, Some(size + 1))
         } else {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use std::fs;
-use json::JsonValue;
-use zendesk_export::{ApiClient, TicketIterator};
+use std::{fs, thread};
+use std::time::Duration;
+use clap::{Arg, Command};
+
+use rayon::prelude::*;
+
+use zendesk_export::ApiClient;
+use zendesk_export::ApiError::UReqError;
 
 fn main() {
-    let config_str = fs::read_to_string("config.toml").unwrap();
+    let mut cmd = Command::new("Zendesk Export")
+        .version("0.1")
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::new("config")
+            .short('c')
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true)
+            .default_value("config.toml"))
+        .arg(Arg::new("tickets")
+            .short('t')
+            .long("tickets")
+            .help("Do fetch tickets")
+            .takes_value(false))
+        .arg(Arg::new("users")
+            .short('u')
+            .long("users")
+            .help("Do fetch users")
+            .takes_value(false));
+    let matches = cmd.get_matches();
+
+
+    let config_str = fs::read_to_string(matches.value_of("config").unwrap()).unwrap();
     let config: toml::Value = toml::from_str(&config_str).unwrap();
     let client: ApiClient = ApiClient::new(
         config.get("login").expect("Expect login in config.toml").as_str().unwrap(),
@@ -11,21 +39,55 @@ fn main() {
         config.get("domain").expect("Expect domain in config.toml").as_str().unwrap(),
     );
 
-    let mut tickets: Vec<JsonValue> = vec![];
-    let tickets_iter = TicketIterator::new(&client);
-    for v in tickets_iter {
-        match v {
-            Ok(val) => {
-                eprint!("Got {} tickets", val["tickets"].len());
-                tickets.extend(val["tickets"].members().map(|v| v.clone()));
+    rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap();
 
-                eprintln!(", total {} tickets", tickets.len());
-            }
-            Err(ref err) => {
-                eprintln!("Some error: {:?}", err);
-            }
-        }
+    if matches.is_present("tickets") {
+        fetch_tickets(&client);
     }
-
-
+    if matches.is_present("users") {
+        fetch_users(&client);
+    }
 }
+
+fn fetch_tickets(client: &ApiClient) {
+    let mut tickets = client.get_all_pages("tickets");
+    tickets
+        .par_iter_mut()
+        .for_each(|ticket| {
+            let ticket_id = ticket["id"].as_u64();
+            match ticket_id {
+                Some(id) => {
+                    let id_str = id.to_string();
+                    loop {
+                        let comments = client.get_comments(&id_str);
+                        match comments {
+                            Ok(val) => {
+                                eprintln!("Got {} comments for {}", val["comments"].len(), &id_str);
+                                ticket["comments"] = val.clone();
+                                break;
+                            }
+                            Err(ref err) => {
+                                eprintln!("Some error: {:?}", err);
+                                if let UReqError(ureq::Error::Status(status, _)) = err {
+                                    if *status == 429 {
+                                        thread::sleep(Duration::from_secs(10));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                None => {
+                    eprintln!("No ticket id");
+                }
+            }
+        });
+
+    fs::write("tickets.json", json::stringify_pretty(tickets, 2)).unwrap();
+}
+
+fn fetch_users(client: &ApiClient) {
+    let users = client.get_all_pages("users");
+    fs::write("users.json", json::stringify_pretty(users, 2)).unwrap();
+}
+
